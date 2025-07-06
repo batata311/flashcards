58520657aa6178e0fd7ba0909c51c53f4ac6fbe7
batata311 <manuella.ferst@escola.pr.gov.br>
2025-07-06 19:08:43 -0300

    mudanças

---
+++ assets/perguntas.js
@@ -0,0 +1,16 @@
+criaCartao(
+    'pão',
+    'o que é preciso para fazer pão?',
+    'farinha. água, fermento e sal'
+)
+criaCartao(
+    'pão',
+    'o que o fermento faz no pão?',
+    'ele faz o pão crescer'
+)
+
+criaCartao(
+    'pão',
+    'o pão precisa descansar antes de assar?',
+    'em média, de 25 a 40 minutos'
+)
